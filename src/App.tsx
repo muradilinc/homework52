fdcdbc519225b9c8a3e836bb6d96d6623c076dc5
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2023-10-20 01:11:35 +0600

    add style framework and config

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,9 @@ const App: React.FC = () => {
   };
 
   return (
-    <div>
-      <button onClick={addCard}>click me</button>
-      <p>{pokerHand()}</p>
+    <div className='text-center mt-20'>
+      <button className='text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-5' onClick={addCard}>Get new cards</button>
+      <p className='text-5xl mb-7 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400'>{pokerHand()}</p>
       <div className='playingCards faceImages'>
         {
           cards?.map((card, index) => {
